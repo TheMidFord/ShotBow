ed18fd28de7934cc748864a16d63409c42209826
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-11-21 09:45:37 -0600

    Gave shotbow rectangular spread

--- src/main/java/midford/shotbow/item/ShotBowItem.java
+++ src/main/java/midford/shotbow/item/ShotBowItem.java
@@ -7,6 +7,7 @@ import net.minecraft.core.entity.projectile.EntityArrowGolden;
 import net.minecraft.core.entity.projectile.EntityArrowPurple;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.util.helper.MathHelper;
 import net.minecraft.core.world.World;
 
 public class ShotBowItem extends Item {
@@ -25,7 +26,9 @@ public class ShotBowItem extends Item {
 				final int shotsToFire = 5;
 				for (int i = 0; i < shotsToFire; i++) {
 					EntityArrow arrow = new SteelArrowEntity(world, entityplayer, true, 0);
-					arrow.setHeading(arrow.xd, arrow.yd, arrow.zd, 1.5f, spread);
+					float hOff = (itemRand.nextFloat() - 0.5f) * 0.4f;
+					float vOff = (itemRand.nextFloat() - 0.5f) * 0.2f;
+					setHeadingDirect(entityplayer, arrow, arrow.xd, arrow.yd, arrow.zd, 1.5f, hOff, vOff);
 					world.entityJoinedWorld(arrow);
 				}
 			}
@@ -35,4 +38,23 @@ public class ShotBowItem extends Item {
 		return itemstack;
 	}
 
+
+	public void setHeadingDirect(EntityPlayer player, EntityArrow arrow, double newMotionX, double newMotionY, double newMotionZ, float speed, float hOff, float vOff) {
+		float velocity = MathHelper.sqrt_double(newMotionX * newMotionX + newMotionY * newMotionY + newMotionZ * newMotionZ);
+
+		float sinYaw = MathHelper.sin(player.yRot * 3.141593F / 180.0F);
+		float cosYaw = MathHelper.cos(player.yRot * 3.141593F / 180.0F);
+
+		newMotionX = (newMotionX / velocity) * speed;
+		newMotionY = (newMotionY / velocity) * speed;
+		newMotionZ = (newMotionZ / velocity) * speed;
+		arrow.xd = newMotionX + (double)(hOff * cosYaw);
+		arrow.yd = newMotionY + vOff;
+		arrow.zd = newMotionZ + (double)(hOff * sinYaw);
+
+
+		float f3 = MathHelper.sqrt_double(newMotionX * newMotionX + newMotionZ * newMotionZ);
+		arrow.yRotO = arrow.yRot = (float)(Math.atan2(newMotionX, newMotionZ) * 180.0 / Math.PI);
+		arrow.xRotO = arrow.xRot = (float)(Math.atan2(newMotionY, f3) * 180.0 / Math.PI);
+	}
 }
